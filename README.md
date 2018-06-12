9b43dbad1b983b4023f24570ef640a6a924fcd14
Andy J. Wills <andy@willslab.co.uk>
2018-06-12 23:22:17 +0100

    First draft of 'goodpapers' script.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # workR
 R scripts to support workload modelling
+
+## Research 
+
+[goodpapers.R](goodpapers.R) - Calculates number of good recent papers published by each staff member, from _Symplectic Elements_ data.
+
+## Updating the data
+Data for these scripts largely has be pulled manually from various other databases. Here's how:
+
+### Publication data
+You need a _research admin_ account on Symplectic Elements to do this. If you don't have such an account, you can still use the last version of the data someone uploaded - just skip to the next section.
+
+1. Go to [Sympletic Elements][1] and log in.
+
+2. Go to Research Admin -> Basic Reports.
+
+3. Go to "Select Groups". Click "+" next to "Faculty of Health and Human Sciences". Tick box by "School of Psychology".
+
+4. Set "Date from:" - **Pick 1st Jan 2014**. 
+
+5. Set "Date to" - **Pick today's date**.
+
+6. Under section 4.i., set _Element_ to "Publication", set _Type_ to "All"", set _Return_ to "Simple rows". Click "Display all fields".
+
+7. Click "Get report". Wait a bit.
+
+8. Save the CSV file you get into the _data_ folder with the name _symplectic-report.csv_. It's important you use this **exact name**.
+
+
+[1]: https://elements.plymouth.ac.uk/login.html
+
+
---
+++ goodpapers.R
@@ -0,0 +1,67 @@
+## goodpapers.R
+## Andy Wills
+## GPL 3.0
+
+## This script returns the number of good, recent, papers produced by each staff member
+## capped to a given maximum
+
+## Specify parameters
+## I've picked ones here that make sense from a REF perspective 
+## i.e. we should give time to the people who are writing our best REF papers
+## but they can't return more than 5, however many they write.
+
+from.year <- 2014 ## The earliest year of publication that counts
+quart.point <- 4  ## The quartile on SNIP that counts as 'good' 2 = LQ, 3 = median, 4 = UQ.
+n.cap <- 5 ## Maximum number of papers (ie. if >ncap then =ncap)
+
+## Load packages
+library(tidyverse)
+
+## Load data
+pubs <- read_csv("data/symplectic-report.csv") 
+
+## Preprocess data
+
+### Select and rename columns we need
+
+needed <- c('ID', 'Username', 'Authors OR Creators', 
+            'Publication date OR Date OR Presentation date OR Date awarded OR Presented date',
+            'Title', 'Journal OR Published proceedings', 'Volume',
+            'Pagination (start page)', 'Times cited (Scopus)',
+            'SNIP rank', 'Status', 'Publication type')
+
+gpubs <- pubs %>% select(needed)  
+
+colnames(gpubs) <-  c('ID', 'Username', 'authors', 'year', 'title','journal',
+                     'vol','page','cites','SNIP', 'status', 'pubtype')
+
+### Kludge to fix kludgy year info from symplectic
+
+gpubs$year <- as.numeric(substr(gpubs$year,1,4))  
+
+### Filter to published journal articles
+gpubs <- gpubs %>% 
+  filter(status %in% c('Published', 'Published online')) %>% 
+  filter(pubtype == "Journal article")
+
+### Remove status and pubtype, as now superfluous
+gpubs <- gpubs %>% select(-status, -pubtype)
+
+## Filter to recent publications
+gpubs <- gpubs %>% filter(year >= from.year)
+
+## Define quartile
+quartiles <- quantile(gpubs$SNIP, na.rm = TRUE)
+qlevel <- quartiles[quart.point]
+
+## Filter to papers above the lower quartile
+gpubs <- gpubs %>% filter(SNIP > qlevel)
+
+## Summarise by username
+gpubsum <- gpubs %>% group_by(Username) %>% summarise(N = n())
+
+## Cap
+gpubsum$N[gpubsum$N > n.cap] <- n.cap
+
+
+
